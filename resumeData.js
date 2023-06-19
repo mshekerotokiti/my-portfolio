d6991c56848f8ce5755ef1607591b32b411b1dec
Sharon Tokiti Mshekero <sharontokiti@gmail.com>
2023-06-19 12:17:57 +0300

    updated resume data

---
+++ resumeData.js
@@ -0,0 +1,115 @@
+let resumeData = {
+    // "imagebaseurl":"https://rbhatia46.github.io/",
+    "name": "Sharon Tokiti Mshekero",
+    "role": "Frontend Developer ",
+    "linkedinId":"https://www.linkedin.com/in/sharon-mshekero?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3B2rIZdhWGRcO9iaeO%2Fcwpdw%3D%3D",
+    // "skypeid": "Your skypeid",
+    "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or try out recipes in my free time.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/sharon-mshekero",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/mshekerotokiti",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"twitter",
+          "url":"https://twitter.com/SharonTokiti",
+          "className":"fa fa-twitter"
+        }
+      ],
+    "aboutme":"I am currently a student at Moringa School and pursuing my certificate in Software Egineering from here. I am learning  Full Stack Web Development, currently diving deeper into backend development using Ruby programming language and Rails framework. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
+    "address":"Nairobi",
+    // "website":"https://rbhatia46.github.io",
+    "education":[
+      {
+        "UniversityName":"Moringa School",
+        "specialization":"Software Engineering",
+        "MonthOfPassing":"Aug",
+        "YearOfPassing":"2023",
+        "Achievements":"Certificate in Software Engineering"
+      },
+      {
+        "UniversityName":"Kenyatta University",
+        "specialization":"Teacher",
+        "MonthOfPassing":"Dec",
+        "YearOfPassing":"2021",
+        "Achievements":"Degree in  Education Arts"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Mission Star Academy",
+        "specialization":"Teacher",
+        "MonthOfLeaving":"Aug",
+        "YearOfLeaving":"2022",
+        "Achievements":"Working with children, Leadership roles"
+      },
+      {
+        "CompanyName":"Pumwani Secondary School",
+        "specialization":"Teacher",
+        "MonthOfLeaving":"Dec",
+        "YearOfLeaving":"2021",
+        "Achievements":"Teaching internship, Team coordination"
+      }
+    ],
+    "skillsDescription":"My skills here",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      },
+      {
+        "skillname":"Ruby"
+      },
+      {
+        "skillname":"Team coordination"
+      },
+      {
+        "skillname":"Leadership"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"Mapishi Simplified",
+        "description":"Meal finder application",
+        "imgurl":"images/portfolio/phone.jpg"
+      },
+    //   {
+    //     "name":"project2",
+    //     "description":"mobileapp",
+    //     "imgurl":"images/portfolio/project.jpg"
+    //   },
+    //   {
+    //     "name":"project3",
+    //     "description":"mobileapp",  
+    //     "imgurl":"images/portfolio/project2.png"
+    //   },
+    //   {
+    //     "name":"project4",
+    //     "description":"mobileapp",
+    //     "imgurl":"images/portfolio/phone.jpg"
+    //   }
+    ],
+    "testimonials":[
+      {
+        "description":"Great Team Member.Works efficiently within a group",
+        "name":"Consolatta Tuwei"
+      },
+    //   {
+    //     "description":"This is a sample testimonial",
+    //     "name":"Some technical guy"
+    //   }
+    ]
+  }
+  
+  export default resumeData;
